95786dc8a38f0b3b940c253f84cec6dcd08b0499
Nurmukhamat <gyazitdinovnurmuhamat@gmail.com>
2025-04-01 23:16:29 +0600

    save

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,19 +1,33 @@
-import "../ComponentCss/Header.css"
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../ComponentCss/Header.css";
+
 export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <header className="header">
-      <div>
-      <nav className="container">
+      {/* Бургер-кнопка */}
+      <div className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Навигация */}
+      <nav className={`container ${menuOpen ? "nav-active" : ""}`}>
         <ul>
-        <li><Link to="/">Главная</Link></li>
-          <li><Link to="/about">О нас</Link></li>
-          <li><Link to="/services">Услуги</Link></li>
-          <li><Link to="/projects">Проекты</Link></li>
-          <li><Link to="/contacts">Связаться с нами</Link></li>
+          <li><Link to="/" onClick={toggleMenu}>Главная</Link></li>
+          <li><Link to="/about" onClick={toggleMenu}>О нас</Link></li>
+          <li><Link to="/services" onClick={toggleMenu}>Услуги</Link></li>
+          <li><Link to="/projects" onClick={toggleMenu}>Проекты</Link></li>
+          <li><Link to="/contacts" onClick={toggleMenu}>Связаться с нами</Link></li>
         </ul>
       </nav>
-      </div>
     </header>
-  )
-}
+  );
+};
